287a411b3214836ef19300e8fb19d1b3bc5b74aa
Sharjeel Faiq <sharjeelfaiq816@gmail.com>
2026-03-14 17:31:29 +0500

    Refactor code structure for improved readability and maintainability

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,47 +1,28 @@
 import express from "express";
-
 import { verifyAccessToken } from "#middleware/validator.js";
+
+// Direct imports for all route modules
+import { healthRoutes, authRoutes, emailRoutes, notificationRoutes, otpRoutes, userRoutes } from "#modules/index.js";
 
 // Parent router
 const appRouter = express.Router();
 
 // Health routes
-appRouter.use("/health", async (req, res, next) => {
-  const { healthRoutes } = await import("#modules/health/index.js");
-  return healthRoutes(req, res, next);
-});
+appRouter.use("/health", healthRoutes);
 
 // V1 router
 const v1Router = express.Router();
-
-appRouter.use("/api/v1", async (req, res, next) => {
-  return v1Router(req, res, next);
-});
+appRouter.use("/api/v1", v1Router);
 
 // V1 routes
-v1Router.use("/auth", async (req, res, next) => {
-  const { authRoutes } = await import("#modules/auth/index.js");
-  return authRoutes(req, res, next);
-});
+v1Router.use("/auth",authRoutes);
 
-v1Router.use("/email", async (req, res, next) => {
-  const { emailRoutes } = await import("#modules/email/index.js");
-  return emailRoutes(req, res, next);
-});
+v1Router.use("/email", emailRoutes);
 
-v1Router.use("/notifications", verifyAccessToken, async (req, res, next) => {
-  const { notificationRoutes } = await import("#modules/notification/index.js");
-  return notificationRoutes(req, res, next);
-});
+v1Router.use("/notifications", verifyAccessToken, notificationRoutes);
 
-v1Router.use("/otp", async (req, res, next) => {
-  const { otpRoutes } = await import("#modules/otp/index.js");
-  return otpRoutes(req, res, next);
-});
+v1Router.use("/otp", otpRoutes);
 
-v1Router.use("/users", verifyAccessToken, async (req, res, next) => {
-  const { userRoutes } = await import("#modules/user/index.js");
-  return userRoutes(req, res, next);
-});
+v1Router.use("/users", verifyAccessToken, userRoutes);
 
 export default appRouter;
